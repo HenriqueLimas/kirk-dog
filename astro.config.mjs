ad570d31bc6cd19164938fdb58002527ea8447cc
HenriqueLimas <henrique.ramos.limas@gmail.com>
2025-12-26 21:28:59 -0800

    update with new domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://henriquelimas.github.io',
-  base: '/kirk-dog',
+  site: "https://kirkbulldog.com",
+  base: "/",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
+
